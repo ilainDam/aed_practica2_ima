538958dc16897b8f54f3259c6e3a4e44278b21da
ilain <jacoboperdon@gmail.com>
2025-09-30 18:26:46 +0100

    Si

---
+++ src/main/java/org/example/InstanciaTeno.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.io.Serializable;
+
+public class InstanciaTeno implements Serializable {
+
+    private String fecha;
+    private int numeroPersonas;
+    private int numeroCoches;
+
+
+}
